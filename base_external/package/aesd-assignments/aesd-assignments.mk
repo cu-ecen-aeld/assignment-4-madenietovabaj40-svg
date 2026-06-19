87f8c550ab4746c257b9cc068a40d2d4fc7fde04
user <user@Ubuntu1.myguest.virtualbox.org>
2026-06-19 05:34:49 +0500

    Configure aesd-assignments.mk to install scripts correctly

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,13 +1,17 @@
-AESD_ASSIGNMENTS_VERSION = '1.0'
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-madenietovabaj40-svg.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_VERSION = 1
+AESD_ASSIGNMENTS_SITE = /home/user/assignment-4-madenietovabaj40-svg/aesd-assignments-source
+AESD_ASSIGNMENTS_SITE_METHOD = local
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	# Настройку компиляции сделаем на следующих шагах
-endef
+# Убираем AESD_ASSIGNMENTS_BUILD_CMDS, так как компилировать код на C нам не из чего
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	# Копирование скриптов в rootfs настроим позже
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
+	# Копируем твой shell-скрипт и переименовываем его просто в writer
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 endef
 
 $(eval $(generic-package))
