9c25cf56a95514a8a7ba748906d3d76390ac11d9
user <user@Ubuntu1.myguest.virtualbox.org>
2026-06-20 15:53:47 +0500

    Update assignment 3 hash to include verified writer.c

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '1a4c18f836be20e501150c077bf0f8f5b6958279'
+AESD_ASSIGNMENTS_VERSION = 'da83530c1f48db4cb31dd7b673c28c2fa7b74700'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-madenietovabaj40-svg.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
